54a70ba0d99dc324367298e65f2ea03524653865
Jun0 <60430391+kriket-kim@users.noreply.github.com>
2025-12-01 02:32:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 100 ms, Memory: 6124 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int arr[1025][1025];
+
+int main() {
+    std::cin.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+
+    int n, m;
+    std::cin >> n >> m;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            int t;
+            std::cin >> t;
+            arr[i][j] = t + arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
+        }
+    }
+    
+    for(int i = 0; i < m; i++){
+        int x1, y1, x2, y2;
+        std::cin >> x1 >> y1 >> x2 >> y2;
+        std::cout << (arr[x2][y2] - arr[x2][y1-1] - arr[x1-1][y2] + arr[x1-1][y1-1]) << "\n";
+    }
+}
